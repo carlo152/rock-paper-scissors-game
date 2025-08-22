33b59a950711ce8d3e1b5c535e743398a7a8e758
Carlos Medina <77945884+carlo152@users.noreply.github.com>
2025-08-22 15:56:58 -0600

    Realiza el proyecto rock-paper-scissors-game

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rock-paper-scissors-game
+
 Make a Rock Paper Scissors game
+
+I made use of:
+  1. Variables
+  2. Conditionals
+  3. Operators
+  4. Functions
+  5. Loops
+
---
+++ javascript.js
@@ -0,0 +1,97 @@
+
+function getComputerChoice() {
+let computerChoice =  Math.floor(Math.random() * 3) + 1;
+
+if (computerChoice == 1) {
+    console.log("Rock"); 
+ } else if (computerChoice == 2) {
+    console.log("Paper");
+ } else {
+    console.log("Scissors");
+ }
+   
+return computerChoice;  
+}
+
+
+
+function getHumanChoice()  {
+    let humanChoice = prompt("Write a number: \n1. Rock \n2. Paper \n3. Scissors");
+
+    if (humanChoice == 1) {
+        console.log("Rock");
+    } else if (humanChoice == 2 ) {
+        console.log("Paper"); 
+    } else if (humanChoice == 3) {
+        console.log("Scissors");
+    } else {
+        console.log("Error");
+    }
+    
+
+    return humanChoice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice == 1) & (computerChoice == 1)){
+        console.log("Play again!");
+    } else if ((humanChoice == 1) & (computerChoice == 2)) {
+        console.log("You lose! Paper beats Rock");
+        computerScore++;
+        console.log("Computer Score = " + computerScore);
+    } else if ((humanChoice == 1) & (computerChoice == 3)) {
+        console.log("You win! Rock beats Scissors");
+        humanScore++;
+        console.log("Your Score = " + humanScore);
+    } else if ((humanChoice == 2) & (computerChoice == 2)){
+        console.log("Play again!");
+    } else if ((humanChoice == 2) & (computerChoice == 1)) {
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+        console.log("Your Score = " + humanScore);
+    } else if ((humanChoice == 2) & (computerChoice == 3)) {
+        console.log("You lose! Scissors beats Paper");
+        computerScore++;
+        console.log("Computer Score = " + computerScore);
+    } else if ((humanChoice == 3) & (computerChoice == 3)){
+        console.log("Play again!");
+    } else if ((humanChoice == 3) & (computerChoice == 1)) {
+        console.log("You lose! Rock beats Scissors");
+        computerScore++;
+        console.log("Computer Score = " + computerScore);
+    } else if ((humanChoice == 3) & (computerChoice == 2)) {
+        console.log("You win! Scissors beats Paper");
+        humanScore++;
+        console.log("Your Score = " + humanScore);
+    } else {
+        console.log("Error");
+    }
+}
+
+
+function playGame() {
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+
+}
+
+for (let i = 0; i < 5; i++) {
+    playGame();
+    
+}
+
+let humanWin = humanScore;
+    let computerWin = computerScore;
+
+    if (humanWin > computerWin) {
+    console.log("Your the winner");
+} else {
+    console.log("Compurer is the winner");
+
+}
